95f8800030fedeafc6646e9c03679a8776ed82f6
cutlerthomas <cutlerthomas0@gmail.com>
2024-12-09 14:16:23 -0700

    testing actions

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -5,8 +5,17 @@ So that I can chat with my spy friends like a pro.
 
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I select the option with the value "1" for element "#shift-amount"
+    When I clear the inputfield "#letters"
+    When I add "Cutler" to the inputfield "#letters"
+    When I click on the button "#submit"
+    Then I expect that element "#decoded_message > p" matches the text "Dvumfs"
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I select the option with the value "D" for element "#decoder-setting"
+    When I select the option with the value "1" for element "#shift-amount"
+    When I clear the inputfield "#letters"
+    When I add "Dvumfs" to the inputfield "#letters"
+    When I click on the button "#submit"
+    Then I expect that element "#decoded_message" matches the text "Cutler"
--- test_brute.py
+++ test_brute.py
@@ -1,18 +1,63 @@
 import pytest
+from unittest.mock import Mock
 from brute import Brute
 
 todo = pytest.mark.skip(reason='todo: pending spec')
 
+@pytest.fixture
+def cracker():
+    return Brute("TDD")
+
+@pytest.fixture
+def mock_randomGuess_correct(mocker):
+    return mocker.patch.object(Brute, 'randomGuess', return_value="TDD")
+
+@pytest.fixture
+def mock_randomGuess_incorrect(mocker):
+    return mocker.patch.object(Brute, 'randomGuess', return_value="LLLL")
+
+@pytest.fixture
+def mock_hash(mocker):
+    return mocker.patch.object(Brute, 'hash', return_value="hash")
+
 def describe_Brute():
 
-    @pytest.fixture
-    def cracker():
-        return Brute("TDD")
-
     def describe_bruteOnce():
-        # write your test cases here
-        pass
+        
+        def it_returns_true_when_attempt_is_correct(cracker):
+            target = cracker
+            assert target.bruteOnce("TDD") == True
+
+        def it_returns_false_when_attempt_is_wrong(cracker):
+            target = cracker
+            assert target.bruteOnce("LLLLLLLLL") == False
+
+        def it_returns_false_when_there_is_extra_whitespace(cracker):
+            target = cracker
+            assert target.bruteOnce(" TDD ") == False
+
+        def it_returns_false_when_caps_are_wrong(cracker):
+            target = cracker
+            assert target.bruteOnce("tdd") == False
 
     def describe_bruteMany():
-        # write your test cases here
-        pass
+        
+        def it_calls_randomGuess_once_when_correct(cracker, mock_randomGuess_correct):
+            target = cracker
+            target.bruteMany()
+            mock_randomGuess_correct.assert_called_once()
+
+        def it_calls_randomGuess_multiple_when_wrong(cracker, mock_randomGuess_incorrect):
+            target = cracker
+            target.bruteMany(limit=1000)
+            mock_randomGuess_incorrect.assert_called_with()
+
+        def it_calls_hash_to_check_guess(cracker, mock_hash):
+            target = cracker
+            target.bruteMany(limit=1000)
+            mock_hash.assert_called()
+
+        def it_passes_correct_value_to_hash(cracker, mock_randomGuess_correct, mock_hash):
+            target = cracker
+            target.bruteMany(limit=1000)
+            mock_hash.asset_called_with("TDD")
